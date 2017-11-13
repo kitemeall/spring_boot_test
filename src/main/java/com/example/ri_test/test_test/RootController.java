41dbcc925859f1172fec7e0522e5953a44a9fe65
rivanov <rivanov@vita-soft.ru>
2017-11-13 18:01:50 +0300

    test

--- src/main/java/com/example/ri_test/test_test/RootController.java
+++ src/main/java/com/example/ri_test/test_test/RootController.java
@@ -1,4 +1,5 @@
 package com.example.ri_test.test_test;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/ri_test/test_test/TestTestApplication.java
+++ src/main/java/com/example/ri_test/test_test/TestTestApplication.java
@@ -2,11 +2,22 @@ package com.example.ri_test.test_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TestTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestTestApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(TestTestApplication.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 }
